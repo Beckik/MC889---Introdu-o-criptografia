71a5a58238545be114c73e13c3827825211afb60
fesoliveira014 <fesoliveira@outlook.com>
2015-03-23 20:50:14 -0300

    comecei programa de crib drag

--- cribbing.cpp
+++ cribbing.cpp
@@ -3,19 +3,21 @@
 #include <fstream>
 #include <vector>
 #include <iterator>
+#include <queue>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if(argc != 3)
+	if(argc != 4)
 	{
-		printf("Invalid input.\n The correct usage is: ./%s input.txt output.txt\n", argv[0]);
+		printf("Invalid input.\n The correct usage is: ./%s input.txt output.txt dictionary.txt\n", argv[0]);
 		return -1;
 	}
 
 	fstream input;
-	fstream output;
+	ofstream output;
+	fstream dictionaryFile;
 
 	input.open(argv[1], fstream::in);
 	if(!input.is_open())
@@ -33,6 +35,14 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	dictionaryFile.open(argv[3], fstream::in);
+	if(!dictionaryFile.is_open())
+	{
+		cout << "Could not load dictionary." << endl;
+		cout << "Exiting..." << endl;
+		return -1;
+	}
+
 	input.seekg(0, ios::end);
 	streampos lenght = input.tellg();
 	input.seekg(0, ios::beg);
@@ -40,22 +50,44 @@ int main(int argc, char* argv[])
 	vector<char> inputText(lenght);
 	input.read(&inputText[0], lenght);
 
-	cout << "Enter the word: ";
-	string word;
-	getline(cin, word);
+	input.close();
 
-	cout << inputText.size() << endl;
-
-	for(int i = 0; i < inputText.size() - 5; i++)
+	priority_queue< string, vector<string>, less<string> > priorityDictionary;
+	vector<string> vectorDictionary;
+	while(!dictionaryFile.eof())
 	{
-		for(int j = 0; j < word.size(); j++)
-		{
-			output.put(word[j] ^ inputText[i + j]);
-		}
-		output << endl;
+		string buffer;
+		dictionaryFile.getline(buffer);
+		priorityDictionary.push(buffer);
+		vectorDictionary.push_back();
 	}
 
-	input.close();
+	dictionaryFile.close()
+
+	for(int i = 0; i < vectorDictionary.size(); i++)
+	{
+		for(int j = 0; j < inputText.size() - vectorDictionary[i].size(); j++)
+		{
+			string wordXOR = "";	
+			for(int k = 0, k < vectorDictionary[i].size(); k++)
+			{
+				wordXOR.append(vectorDictionary[i][k] ^ inputText[j + k]);
+			}
+
+			for(int k = 0; k < priorityDictionary.size(); k++)
+			{
+				if(priorityDictionary[k].compare(wordXOR))
+					{
+						output << wordXOR << endl;
+						break;
+					}
+			}
+			
+		}
+
+
+	}	
+
 	output.close();
 
 	return 0;
